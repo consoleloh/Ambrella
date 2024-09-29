7cb89f6bcfc9044bf445bf344f6bf465c088cdbc
consoleloh <freekeyreal@gmail.com>
2024-09-29 10:57:55 +0300

    section hero

--- src/js/script.js
+++ src/js/script.js
@@ -73,8 +73,13 @@
 
     const collapseProblem = () => {
         document.querySelectorAll('.js-problem-collapse').forEach(problem => {
-            problem.addEventListener('click', e => {
+            const control = problem.querySelector('.problem__title');
+
+            control.addEventListener('click', e => {
+                const text = problem.querySelector('.problem__text');
+
                 problem.classList.toggle('problems__item--open');
+                slideToggle(text, 300);
             });
         });
     }
@@ -117,6 +122,69 @@
 
         window.addEventListener('load', slider);
         window.addEventListener('resize', slider);
+    };
+    
+    const slideUp = (target, duration = 500) => {
+        target.style.transitionProperty = 'height, margin, padding';
+        target.style.transitionDuration = duration + 'ms';
+        target.style.boxSizing = 'border-box';
+        target.style.height = target.offsetHeight + 'px';
+        target.offsetHeight;
+        target.style.overflow = 'hidden';
+        target.style.height = 0;
+        target.style.paddingTop = 0;
+        target.style.paddingBottom = 0;
+        target.style.marginTop = 0;
+        target.style.marginBottom = 0;
+        window.setTimeout(() => {
+            target.style.display = 'none';
+            target.style.removeProperty('height');
+            target.style.removeProperty('padding-top');
+            target.style.removeProperty('padding-bottom');
+            target.style.removeProperty('margin-top');
+            target.style.removeProperty('margin-bottom');
+            target.style.removeProperty('overflow');
+            target.style.removeProperty('transition-duration');
+            target.style.removeProperty('transition-property');
+        }, duration);
+    };
+     
+    const slideDown = (target, duration = 500) => {
+        target.style.removeProperty('display');
+
+        let display = window.getComputedStyle(target).display;
+     
+        if (display === 'none') display = 'block';
+        
+        target.style.display = display;
+
+        const height = target.offsetHeight;
+
+        target.style.overflow = 'hidden';
+        target.style.height = 0;
+        target.style.paddingTop = 0;
+        target.style.paddingBottom = 0;
+        target.style.marginTop = 0;
+        target.style.marginBottom = 0;
+        target.offsetHeight;
+        target.style.boxSizing = 'border-box';
+        target.style.transitionProperty = "height, margin, padding";
+        target.style.transitionDuration = duration + 'ms';
+        target.style.height = height + 'px';
+        target.style.removeProperty('padding-top');
+        target.style.removeProperty('padding-bottom');
+        target.style.removeProperty('margin-top');
+        target.style.removeProperty('margin-bottom');
+        window.setTimeout(() => {
+            target.style.removeProperty('height');
+            target.style.removeProperty('overflow');
+            target.style.removeProperty('transition-duration');
+            target.style.removeProperty('transition-property');
+        }, duration);
+    };
+
+    const slideToggle = (target, duration = 500) => {
+        return window.getComputedStyle(target).display === 'none' ? slideDown(target, duration) : slideUp(target, duration);
     };
 
     checkWEBP();
